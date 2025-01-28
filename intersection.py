254d587ac68502585f8c9928a58a0bdb4f21a558
Sergey <sergionik03@gmail.com>
2025-01-28 17:23:59 +0300

    commit

--- intersection.py
+++ intersection.py
@@ -46,28 +46,36 @@ class CutIntersection:
         
         H = self.__homography()
 
-        # transform_image1 = cv2.warpPerspective(self.image1, H, (self.image2.shape[1], self.image2.shape[0]))
-        self.transform_image1 = self.image1
-
         h, w, _ = self.image1.shape
 
-        corners = np.array([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]], dtype='float32')
+        self.transform_image1 = cv2.warpPerspective(self.image1, H, (w, h))
+        # self.transform_image1 = self.image1
 
-        transform_corners = cv2.perspectiveTransform(corners.reshape(-1, 1, 2), H).reshape(-1, 2)
 
-        inv_corners = cv2.perspectiveTransform(corners.reshape(-1, 1, 2), np.linalg.inv(H)).reshape(-1, 2)
+        self.corners = self.create_corners(w, h)
 
+        self.transform_corners = cv2.perspectiveTransform(self.corners.reshape(-1, 1, 2), H).reshape(-1, 2)
+
+        self.inv_corners = cv2.perspectiveTransform(self.transform_corners.reshape(-1, 1, 2), np.linalg.inv(H)).reshape(-1, 2)
+        self.inv_corners_int = np.int32(self.inv_corners)
         mask_image2 = np.zeros_like(self.image2)
-        cv2.fillConvexPoly(mask_image2, np.int32(transform_corners), (255, 255, 255))
+        cv2.fillConvexPoly(mask_image2, np.int32(self.transform_corners), (255, 255, 255))
+        self.transform_image2 = cv2.bitwise_and(self.image2, mask_image2)     
 
-        self.transform_image2 = cv2.bitwise_and(self.image2, mask_image2)
+        self.transform_image1 = cv2.warpPerspective(self.transform_image1, np.linalg.inv(H), (w, h))
+        self.transform_image2 = cv2.warpPerspective(self.transform_image2, np.linalg.inv(H), (self.image1.shape[1], self.image1.shape[0]))
 
-        # transform_image1 = cv2.warpPerspective(transform_image1, np.linalg.inv(H), (w, h))
-        self.transform_image2 = cv2.warpPerspective(self.transform_image2, np.linalg.inv(H), (w, h))
+        self.find_black_area()
 
-        self.show_result()    
+        self.show_result()
+
+        cv2.imwrite('res_image1.jpg', self.transform_image1)
+        cv2.imwrite('res_image2.jpg', self.transform_image2)
 
     def show_result(self):
+
+        # cv2.polylines(self.image2, [np.int32(self.t)], isClosed=True, color=(0, 255, 0), thickness=3)
+
         plt.figure(figsize=(15, 10))
 
         plt.subplot(1, 3, 1)
@@ -87,4 +95,56 @@ class CutIntersection:
 
         plt.show()
 
-CutIntersection('pairs/5/camera.jpg', 'pairs/5/kvadra.jpg').find_area()
+    def find_black_area(self):
+        h, w, _ = self.transform_image2.shape
+        corners = self.create_corners(w, h)
+        s1, p1 = corners[0], self.inv_corners_int[0]
+        s2, p2 = corners[1], self.inv_corners_int[1]
+        s3, p3 = corners[2], self.inv_corners_int[2]
+        s4, p4 = corners[3], self.inv_corners_int[3]
+
+        if p1[0] > s1[0]:
+            self.transform_image1 = self.transform_image1[:, p1[0]:]
+            self.transform_image2 = self.transform_image2[:, p1[0]:]
+            print(1)
+        if p1[1] > s1[1]:
+            self.transform_image1 = self.transform_image1[p1[1]:, :]
+            self.transform_image2 = self.transform_image2[p1[1]:, :]
+            print(2)
+        if p2[0] > s2[0]:
+            self.transform_image1 = self.transform_image1[:, p2[0]:]
+            self.transform_image2 = self.transform_image2[:, p2[0]:]
+            print(3)
+        if p2[1] < s2[1]:
+            self.transform_image1 = self.transform_image1[:p2[1], :]
+            self.transform_image2 = self.transform_image2[:p2[1], :]
+            print(4)
+        if p3[0] < s3[0]:
+            self.transform_image1 = self.transform_image1[:, :p3[0]]
+            self.transform_image2 = self.transform_image2[:, :p3[0]]
+            print(5)
+        if p3[1] < s3[1]:
+            self.transform_image1 = self.transform_image1[:p3[1], :]
+            self.transform_image2 = self.transform_image2[:p3[1], :]
+            print(6)
+        if p4[0] < s4[0]:
+            self.transform_image1 = self.transform_image1[:, :p4[0]]
+            self.transform_image2 = self.transform_image2[:, :p4[0]]
+            print(7)
+        if p4[1] > s4[1]:
+            self.transform_image1 = self.transform_image1[p4[1]:, :]
+            self.transform_image2 = self.transform_image2[p4[1]:, :]
+            print(8)
+
+    def show_img(self, img):
+        plt.subplot(1, 1, 1)
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        plt.show()
+
+    def create_corners(self, w, h):
+        return np.array([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]], dtype='float32')
+
+a = CutIntersection('pairs/8/camera.jpg', 'pairs/8/kvadra.jpg')
+a.find_area()
+# a.find_black_area()
